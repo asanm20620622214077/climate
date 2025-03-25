0a1fae7469b0cddf191ff5139820ad804f271717
asanm20620622214077 <144363586+asanm20620622214077@users.noreply.github.com>
2025-03-25 21:06:51 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,24 @@
+<html>
+    <head>
+        <script>
+            function speak() {
+                alert('Hello')
+            }
+        </script>
+    </head>
+    <body>
+        <button onclick="speak()">Click Me</button>
+    </body>
+<head>
+    <title> </title>
+    <script>
+        function showHiddenText() {
+        document.getElementById("demo").innerHTML = "Hello World";
+      }
+    </script>
+</head>
+<body>
+    <button onclick="showHiddenText()">Click me</button>
+    <p id="demo"></p>
+
+</body></html>
